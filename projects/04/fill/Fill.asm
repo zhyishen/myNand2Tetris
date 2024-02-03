348463c83d98f4537ee3cdd4f71b42320ccc83c8
zhyishen <zhyishen@163.com>
2024-02-03 22:41:51 +0800

    finish fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+@8192
+D = A;
+@R1
+M = D; // R1 = 8192
+
+(LOOP)
+@KBD
+D = M;
+@WHITE
+D;JEQ //KBD == 0; JMP WHITE
+@BLACK
+D;JNE //KBD != 0; JMP BLACK
+@LOOP 
+0;JMP
+
+(BLACK)
+@i
+M = 0; //i = 0;
+(BLACKLOOP)
+@R1
+D = M;
+@i
+D = D - M; 
+@LOOP
+D;JLE //8192-i <=0; JMP LOOP
+@SCREEN
+D = A;
+@i
+A = D + M;
+M = -1; //RAM[SCREEN + I] = -1;
+@i
+M = M+1;//i +=1;
+@BLACKLOOP
+0;JMP
+
+(WHITE)
+@i
+M = 0; //i = 0;
+(WHITELOOP)
+@R1
+D = M;
+@i
+D = D - M; 
+@LOOP
+D;JLE //8192-i <=0; JMP LOOP
+@SCREEN
+D = A;
+@i
+A = D + M;
+M = 0; //RAM[SCREEN + I] = 0;
+@i
+M = M+1;//i +=1;
+@WHITELOOP
+0;JMP
